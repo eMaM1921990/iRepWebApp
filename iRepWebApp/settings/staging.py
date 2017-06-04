0aea6a2d5bc0c548cc000052eef53010746ae3fc
ahmed_elemam <emam151987@gmail.com>
2017-06-04 21:26:30 +0200

    Godaddy Db init connections

--- iRepWebApp/settings/staging.py
+++ iRepWebApp/settings/staging.py
@@ -2,10 +2,10 @@ from base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'irep',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',  # '127.0.0.1'
-        'PORT': '5432',  # '5556'
+        'NAME': 'djangostack',
+        'HOST': '/opt/bitnami/postgresql',
+        'PORT': '5432',
+        'USER': 'bitnami',
+        'PASSWORD': '31f50ea6e1'
     }
 }
