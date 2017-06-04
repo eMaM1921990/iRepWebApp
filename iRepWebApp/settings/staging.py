08f8180d0f621cd85ca9c3d7edd1b3c2f3737380
ahmed_elemam <emam151987@gmail.com>
2017-06-04 21:42:13 +0200

    Wsgi staging

--- iRepWebApp/settings/staging.py
+++ iRepWebApp/settings/staging.py
@@ -12,3 +12,34 @@ DATABASES = {
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
+
+# LOGGING
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'irep_log.log')',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'iRep': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
