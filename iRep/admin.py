9eedbe39f591b45178c0f4f593ccaa9dd5ec7491
ahmed_elemam <emam151987@gmail.com>
2017-09-30 09:27:07 +0200

    forms save form retrieve API

--- iRep/admin.py
+++ iRep/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from iRep.models import SalesForceCategory, ProductGroup, Product, ProductUnit, AppLanguage, Client, SalesFunnelStatus, \
-    SalesForce, SalesForceTrack, SalesForceCheckInOut, Tags
+    SalesForce, SalesForceTrack, SalesForceCheckInOut, Tags, Forms
 
 admin.site.register(SalesForceCategory)
 admin.site.register(ProductGroup)
@@ -17,3 +17,4 @@ admin.site.register(SalesForce)
 admin.site.register(SalesForceTrack)
 admin.site.register(SalesForceCheckInOut)
 admin.site.register(Tags)
+admin.site.register(Forms)
