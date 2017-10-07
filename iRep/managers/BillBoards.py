ece351174700da69015ccf7f8fbca2dd1c04109b
ahmed_elemam <emam151987@gmail.com>
2017-10-07 15:15:03 +0200

    bill board

--- iRep/managers/BillBoards.py
+++ iRep/managers/BillBoards.py
@@ -1,5 +1,3 @@
-from django.conf.global_settings import DATE_INPUT_FORMATS
-
 from iRep.models import BillBoard
 import datetime
 
@@ -17,6 +15,6 @@ class BillBoards:
         return BillBoard.objects.get(id=id)
 
     def listToday(self):
-        currentDate = datetime.datetime.strptime(self.from_date, DATE_INPUT_FORMATS[0])
+        currentDate = datetime.datetime.today()
         return BillBoard.objects.filter(corporate__slug=self.slug, created_date__day=currentDate.day,
                                         created_date__month=currentDate.month, created_date__year=currentDate.year)
