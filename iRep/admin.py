8bc256f47f2a012c393e2b408610c51672d8f905
ahmed_elemam <emam151987@gmail.com>
2017-10-10 22:39:42 +0200

    clients

--- iRep/admin.py
+++ iRep/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from iRep.models import SalesForceCategory, ProductGroup, Product, ProductUnit, AppLanguage, Client, SalesFunnelStatus, \
-    SalesForce, SalesForceTrack, SalesForceCheckInOut, Tags, Forms, Orders
+    SalesForce, SalesForceTrack, SalesForceCheckInOut, Tags, Forms, Orders, OrderLine
 
 admin.site.register(SalesForceCategory)
 admin.site.register(ProductGroup)
@@ -19,3 +19,4 @@ admin.site.register(SalesForceCheckInOut)
 admin.site.register(Tags)
 admin.site.register(Forms)
 admin.site.register(Orders)
+admin.site.register(OrderLine)
