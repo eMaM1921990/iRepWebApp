128e0726dcc240259996d0b09bf7059316e8fd5f
ahmed_elemam <emam151987@gmail.com>
2017-10-11 21:33:21 +0200

    dashboard activity

--- iRep/managers/Reports.py
+++ iRep/managers/Reports.py
@@ -58,8 +58,9 @@ class TrackingReports():
 
     def tracking_sales_force_by_corp(self, slug):
         currentDate = datetime.datetime.strptime(self.from_date, DATE_INPUT_FORMATS[0])
-        return SalesForceTrack.objects.filter(sales_force__corp_id=slug,created_date__day=currentDate.day,
-                                              created_date__month=currentDate.month,created_date__year=currentDate.year)
+        return SalesForceTrack.objects.filter(sales_force__corp_id=slug, created_date__day=currentDate.day,
+                                              created_date__month=currentDate.month,
+                                              created_date__year=currentDate.year)
 
 
 class DashBoardReports():
@@ -74,10 +75,12 @@ class DashBoardReports():
         self.context['activeRep'] = self.get_active_salesforce_count()
         self.context['clientCount'] = self.get_create_clients_count()
         self.context['total_order'] = self.get_total_orders()
-        self.context['total_amount']  = self.get_total_amount()
+        self.context['total_amount'] = self.get_total_amount()
         self.context['totalSchedual'] = self.get_total_schedual()
         self.context['totalVisit'] = self.get_total_visits()
-        self.context['missed'] = self.context['totalSchedual']-self.context['totalVisit']
+        self.context['missed'] = self.context['totalSchedual'] - self.context['totalVisit']
+        self.context['follow_up'] = self.get_follow_up_client()
+        self.context['new_client'] = self.get_new_client
 
     def get_active_salesforce_count(self):
         return SalesForceTimeLine.objects.filter(time_line_date=self.from_date, end_time__isnull=True,
@@ -94,24 +97,42 @@ class DashBoardReports():
         return Orders.objects.filter(sales_force__corp_id=self.corp, order_date__day=start_date.day,
                                      order_date__month=start_date.month, order_date__year=start_date.year).count()
 
-
     def get_total_amount(self):
         start_date = datetime.datetime.strptime(self.from_date, DATE_INPUT_FORMATS[0])
         return (Orders.objects.filter(sales_force__corp_id=self.corp, order_date__day=start_date.day,
-                                     order_date__month=start_date.month, order_date__year=start_date.year).aggregate(Sum('total')))['total__sum']
+                                      order_date__month=start_date.month, order_date__year=start_date.year).aggregate(
+            Sum('total')))['total__sum']
 
     def get_visit_details_charts(self):
-        return Visits.objects.filter(visit_date__lte=self.to_date,visit_date__gte = self.from_date,sales_force__corp_id=self.corp).values('visit_date').annotate(
-            totalVisit = Count('visit_date')
+        return Visits.objects.filter(visit_date__lte=self.to_date, visit_date__gte=self.from_date,
+                                     sales_force__corp_id=self.corp).values('visit_date').annotate(
+            totalVisit=Count('visit_date')
         )
 
     def get_total_orders_chart(self):
-        return Orders.objects.filter(order_date__lte=self.to_date,order_date__gte=self.from_date,sales_force__corp_id=self.corp).values('order_date').annotate(
-            totalOrder = Count('order_date'), totalAmount = Sum('total')
+        return Orders.objects.filter(order_date__lte=self.to_date, order_date__gte=self.from_date,
+                                     sales_force__corp_id=self.corp).values('order_date').annotate(
+            totalOrder=Count('order_date'), totalAmount=Sum('total')
         )
 
     def get_total_schedual(self):
-        return SalesForceSchedual.objects.filter(schedual_date__lte=self.to_date,schedual_date__gte=self.from_date,sales_force__corp_id=self.corp).count()
+        return SalesForceSchedual.objects.filter(schedual_date__lte=self.to_date, schedual_date__gte=self.from_date,
+                                                 sales_force__corp_id=self.corp).count()
 
     def get_total_visits(self):
-        return Visits.objects.filter(visit_date__lte=self.to_date,visit_date__gte=self.from_date,sales_force__corp_id=self.corp).count()
+        return Visits.objects.filter(visit_date__lte=self.to_date, visit_date__gte=self.from_date,
+                                     sales_force__corp_id=self.corp).count()
+
+    def get_follow_up_client(self):
+        return Orders.objects.filter(order_date__lte=self.to_date, order_date__gte=self.from_date,
+                                     sales_force__corp_id=self.corp).count()
+
+    def get_qualificed_client(self):
+        return Visits.objects.filter(visit_date__lte=self.to_date, visit_date__gte=self.from_date,
+                                     sales_force__corp_id=self.corp)
+
+    def get_new_client(self):
+         visit = Visits.objects.filter(visit_date__lte=self.to_date, visit_date__gte=self.from_date,
+                              sales_force__corp_id=self.corp)
+
+         return Client.objects.all().exclude(id__in = [v.branch.id for v in visit])
